4a663c9759c30557fdaf4772cb6d9d83731a6321
Niklas Arnitz <niklas.arnitz@jamitlabs.com>
2019-10-04 15:47:50 +0200

    update

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
 import db from './app/dummy_db/db'
 
 // Set up express app
 const app = express()
-
-// parse requests of content-type - application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: true }))
-
-// Configuring the database
-// const dbConfig = require('./app/config/database.config.js');
-// const mongoose = require('mongoose');
-
-// mongoose.Promise = global.Promise;
-
-// Connecting to the database
-// mongoose.connect(dbConfig.url, {
-//     useNewUrlParser: true
-// }).then(() => {
-//     console.log("Successfully connected to the database");    
-// }).catch(err => {
-//     console.log('Could not connect to the database. Exiting now...', err);
-//     process.exit();
-// });
 
 // Set port the api should listen on
 const port = 9000
@@ -42,11 +24,18 @@ app.get('/api/v1/ezcasts', (req, res) => {
 app.get('/api/v1/status', (req, res) => {
 	res.status(200).send({
 		success: true,
-		message: 'No errors in backend detected.'
+		message: 'No errors in backend detected.',
 		version: '0.0.1'
 	})
 })
 
-app.listen(port, () => {
+mongoose.connect('mongodb://localhost:27017/ezcast-snmp')
+  .then(() => {
+    console.log('mongodb started.');
+    app.listen(port, () => {
 	console.log('Server is now running API and listening on port ' + port)
-})
+	})
+  }).catch(() => {
+    console.log('Mongodb connection failed.');
+  })
+
