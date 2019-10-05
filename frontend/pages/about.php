4128be4eeaf681b227920f6104ac79e12eee7407
Domi04151309 <domi04151309business@gmail.com>
2019-10-05 11:09:18 +0200

    Add content to about.php

--- frontend/pages/about.php
+++ frontend/pages/about.php
@@ -8,3 +8,14 @@ if (!isset($TPL)) {
   exit;
 }
 ?>
+<section class="card-margin">
+  <img id="image-first" alt="The letter I in a circle" src="/images/ic_color_about.svg" width="48px">
+  <h3>About</h3>
+  <p>
+    A responsive web interface for deploying, managing and maintaining multiple EZCast Pro II Sticks.<br>
+    This website was originally developed for the WHG-Durmersheim.
+    <br><br>
+    <button onclick="location.href='https://github.com/WHG-Durmersheim/ezcast-snmp'" type="button">Repository</button>
+    <button onclick="location.href='https://github.com/WHG-Durmersheim/ezcast-snmp/blob/master/CONTRIBUTORS.MD'" type="button">Contributors</button>
+  </p>
+</section>
