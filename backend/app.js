1b42bb76b9ead27bce888a4fbcced05b707a08bd
Niklas Arnitz <niklas.arnitz@jamitlabs.com>
2019-10-08 18:39:26 +0200

    Add Routers

--- backend/app.js
+++ backend/app.js
@@ -8,12 +8,17 @@ const app = express()
 // Set port the api should listen on
 const port = 9000
 
+// Add Routers
+require('./app/routes/configs.routes.js')(app);
+require('./app/routes/ezcasts.routes.js')(app);
+require('./app/routes/users.routes.js')(app);
+
 mongoose.connect('mongodb://localhost:27017/ezcast-snmp', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 })
   .then(() => {
-    console.log('mongodb started.');
+    console.log('Connection to mongoDB was successfully made.');
     app.listen(port, () => {
 	console.log('Server is now running API and listening on port ' + port)
 	})
