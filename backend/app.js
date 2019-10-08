e3872e46588e7ee8989ddcb607900eea7ce52ca5
Niklas Arnitz <niklas.arnitz@jamitlabs.com>
2019-10-08 17:50:17 +0200

    Remove DummyDB

--- backend/app.js
+++ backend/app.js
@@ -2,35 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
-import db from './app/dummy_db/db'
-
 // Set up express app
 const app = express()
 
 // Set port the api should listen on
 const port = 9000
 
-// Get all EZCasts (Currently Dummy data)
-app.get('/api/v1/ezcasts', (req, res) => {
-	res.setHeader('Access-Control-Allow-Origin', '*')
-	res.status(200).send({
-		success: 'true',
-		message: 'ezcasts retrieved successfully',
-		ezcasts: db
-	})
+mongoose.connect('mongodb://localhost:27017/ezcast-snmp', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 })
-
-// Get version of Backend
-app.get('/api/v1/status', (req, res) => {
-	res.setHeader('Access-Control-Allow-Origin', '*')
-	res.status(200).send({
-		success: true,
-		message: 'No errors in backend detected.',
-		version: '0.0.1'
-	})
-})
-
-mongoose.connect('mongodb://localhost:27017/ezcast-snmp')
   .then(() => {
     console.log('mongodb started.');
     app.listen(port, () => {
@@ -39,4 +20,3 @@ mongoose.connect('mongodb://localhost:27017/ezcast-snmp')
   }).catch(() => {
     console.log('Mongodb connection failed.');
   })
-
